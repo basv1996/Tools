9872d5e4b18e182ca7921678fbff54abf4875904
basv1996 <bas.vugts@gmail.com>
2016-10-18 11:50:16 +0200

    commit 2

---
+++ js/modaalStart.js
@@ -0,0 +1,23 @@
+$(document).ready( function() {
+    var herfst1Inhoud = $('#herfst1').detach();
+    var herfst2Inhoud = $('#herfst2').detach();
+    var herfst3Inhoud = $('#herfst3').detach();
+    var herfst4Inhoud = $('#herfst4').detach();
+
+    
+   $('#herfst1Thumb').on('click', function() {
+       DePopUP.openen({inhoud: herfst1Inhoud, breedte: 600});
+   });
+    
+    $('#herfst2Thumb').on('click', function() {
+       DePopUP.openen({inhoud: herfst2Inhoud, breedte: 600});
+   });
+      
+    $('#herfst3Thumb').on('click', function() {
+       DePopUP.openen({inhoud: herfst3Inhoud, breedte: 600});
+   });  
+    
+    $('#herfst4Thumb').on('click', function() {
+       DePopUP.openen({inhoud: herfst4Inhoud, breedte: 600});
+   });  
+});
---
+++ js/modaalVenster.js
@@ -0,0 +1,37 @@
+var DePopUP = (function() { 
+    var $window = $(window);
+    var $Venster = $('<div class="modaal-venster"/>');
+    var $Content = $('<div class="modaal-inhoud"/>');
+    var $Kruisje = $('<span class="kruisje">&#10005;</span>');
+    
+    $Venster.append($Content);
+    
+    return {
+        centreren: function() {
+            var links = Math.max($window.outerWidth()-$Content.width(), 0)/2;
+            var boven = Math.max($window.outerHeight()-$Content.height(), 0)/2;
+            $Content.css({left: links, top: boven});
+            
+        },
+        openen: function(instellingen) {
+            $Content.append(instellingen.inhoud, $Kruisje);
+            $Content.css({width: instellingen.breedte+'px' || 'auto', height: instellingen.hoogte+'px' || 'auto'})
+                .on('click', function(e){
+            e.stopPropagation();
+            });
+            $Venster.appendTo('body')
+            .on('click', DePopUP.sluiten);
+            DePopUP.centreren();
+            $Kruisje.on('click', DePopUP.sluiten);
+            $window.on('resize', DePopUP.centreren);
+        },
+        sluiten: function(){
+            $Content.empty()
+            .off('click', DePopUP.sluiten);
+            $Venster.detach();
+            $window.off('resize', DePopUP.centreren);
+            
+            
+        }
+    }  
+}());
